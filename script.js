def10d11723213082d889dd16ea559a841d71a55
EitanD956 <4hrzgy7y8b@privaterelay.appleid.com>
2025-12-11 16:52:37 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const form = document.getElementById("bookingForm");
+const msg = document.getElementById("resultMsg");
 
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    msg.textContent = "Sending...";
+
+    const formData = new FormData(form);
+
+    const res = await fetch(form.action, {
+        method: "POST",
+        body: formData,
+        headers: { "Accept": "application/json" }
+    });
+
+    if (res.ok) {
+        msg.textContent = "Appointment request sent!";
+        form.reset();
+    } else {
+        msg.textContent = "Something went wrong. Try again.";
+    }
+});
